18dd97fa9a984461fc5cc7492ebd8b203cb84843
SROTH <simon.roth@ynov.com>
2023-03-31 12:05:42 +0200

    modif table ExerciceDone

--- database/DB.sql
+++ database/DB.sql
@@ -1,26 +1,28 @@
-CREATE TABLE IF NOT EXISTS Users(
-    IdUser INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    Progression INTEGER NOT NULL,
-    Name TEXT(30) NOT NULL,
-    Email TEXT(255) NOT NULL,
-    Pwd TEXT(255) NOT NULL,
-    Score INTEGER NOT NULL,
-    FOREIGN KEY (Progression) REFERENCES ExerciceDone(Progression)
+
+CREATE TABLE IF NOT EXISTS Users
+(
+    IdUser      INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Progression INTEGER                           NOT NULL,
+    Name        TEXT(30)                          NOT NULL,
+    Email       TEXT(255)                         NOT NULL,
+    Pwd         TEXT(255)                         NOT NULL,
+    Score       INTEGER                           NOT NULL,
+    FOREIGN KEY (Progression) REFERENCES ExerciceDone (Progression)
 );
 
-CREATE TABLE IF NOT EXISTS Exercices(
-    IdExo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    Progression INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    NameExo TEXT(30) NOT NULL,
-    Description TEXT(255) NOT NULL,
-    Difficulte INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS Exercices
+(
+    IdExo       INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Progression INTEGER                           NOT NULL,
+    NameExo     TEXT(30)                          NOT NULL,
+    Description TEXT(255)                         NOT NULL,
+    Difficulte  INTEGER                           NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS ExerciceDone(
-    IdExo INTEGER NOT NULL ,
-    NameExo TEXT(30) NOT NULL,
-    Progression INTEGER NOT NULL,
-    FOREIGN KEY (Progression) REFERENCES Exercices(Progression),
-    FOREIGN KEY (NameExo) REFERENCES Exercices(NameExo),
-    FOREIGN KEY (IdExo) REFERENCES Exercices(IdExo)
+CREATE TABLE IF NOT EXISTS ExerciceDone
+(
+    IdExo       INTEGER  NOT NULL,
+    Progression INTEGER  NOT NULL,
+    FOREIGN KEY (Progression) REFERENCES Exercices (Progression),
+    FOREIGN KEY (IdExo) REFERENCES Exercices (IdExo)
 );
